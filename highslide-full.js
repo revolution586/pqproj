1c5dc70e30f945f0207ece2dcc3a6a69073f2ee2
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-07-31 23:51:11 -0700

    Update highslide-full.js

--- highslide-full.js
+++ highslide-full.js
@@ -1,1 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<SCRIPT>
+	Image1 = new Image (1680, 1050)
+Image1.src = "/images/circuit_art-wallpaper-1680x1050.jpg"
+Image1 = new Image (1680, 1050)
+Image1.src = "/images/circuit_art-wallpaper-1680x1050.jpg"
 
+// End Hiding -->
+</SCRIPT>
+
+		
