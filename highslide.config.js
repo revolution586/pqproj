5d4d4294362c9d99ca5f61808d84615a4fcebbcf
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-08-01 00:54:26 -0700

    Update highslide.config.js

--- highslide.config.js
+++ highslide.config.js
@@ -1,1 +1,16 @@
-
+// Site-specific configuration settings for Highslide JS //
+hs.graphicsDir = '/highslide/graphics/';
+hs.showCredits = false;
+hs.outlineType = 'custom';
+hs.dimmingOpacity = .7;
+hs.dimmingDuration = 200;
+hs.align = 'center';
+hs.allowMultipleInstances = false;
+hs.blockRightClick = true;
+hs.registerOverlay({
+  html: '<div class="closebutton" onclick="return hs.close(this)" title="Close"></div>',
+  position: 'top right',
+  useOnHTML: true,
+  fade: 2 // fading the semi-transparent overlay looks bad in IE
+});
+  
